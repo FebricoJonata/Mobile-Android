17240bb0f0e52117e9feded3dc9f027283b17a8e
FebricoJ <riko967gmail.com>
2023-04-28 14:46:44 +0700

    Bottom Navigation menu with fragment

---
+++ Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottom_nav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ArticleFragment()).commit();
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.article:
+                        selectedFragment = new ArticleFragment();
+                        break;
+
+                    case R.id.ticket:
+                        selectedFragment = new TicketFragment();
+                        break;
+                    case R.id.history:
+                        selectedFragment = new HistoryFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
