bc00b83cd3af43ec5def05083429cda03029236e
FebricoJ <riko967gmail.com>
2023-07-06 16:57:26 +0700

    Menu with fragment done

--- API/app/src/main/java/com/example/uas/MainActivity.java
+++ API/app/src/main/java/com/example/uas/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.uas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottom_nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        bottom_nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        selectedFragment = new HomeFragment();
+                        break;
+
+                    case R.id.notification:
+                        selectedFragment = new NotificationFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+
+    }
+
+    void init(){
+        bottom_nav = findViewById(R.id.bottom_nav);
     }
 }
