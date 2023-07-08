a9f3e1f1bbb59d046feadaf5df9b44db17f946b2
FebricoJ <riko967gmail.com>
2023-07-08 13:36:31 +0700

    Finalize

--- API/app/src/main/java/com/example/uas/FCM/FCMNotification.java
+++ API/app/src/main/java/com/example/uas/FCM/FCMNotification.java
@@ -1,17 +1,8 @@
 package com.example.uas.FCM;
 
-import android.nfc.Tag;
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.example.uas.database.Database;
-import com.example.uas.database.NotificationHelper;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -24,16 +15,10 @@ public class FCMNotification extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
-        System.out.println("Test");
-        System.out.println("Title : " + message.getNotification().getTitle());
-        System.out.println("Body  : " + message.getNotification().getBody());
         String title = message.getNotification().getTitle();
         String body  = message.getNotification().getBody();
         db = Database.getInstance(this);
         db.addNotification(title, body);
-        System.out.println("SUKSES");
     }
-
-
 
 }
